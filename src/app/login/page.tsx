58aa6a99ac6852fffbdc522246b055543057bec4
Mikael Espínola <mikaelespinolaa@gmail.com>
2025-04-10 15:05:48 -0300

    :sparkles: feat: estrutura calendário incrementado

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const page = () => {
     return (
-        <div>
+        <div className='p-2'>
             <LinkButton text='Voltar' href='/' />
             <h1>
                 Login
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import CalendarioFixo from "@/components/calendarioFixo/CalendarioFixo";
 import Footer from "@/components/footer/Footer";
 import LinkButton from "@/components/linkButton/LinkButton";
 import Link from "next/link";
@@ -5,12 +6,13 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <main className="m-0.5 border-b">
+      <main className="p-2 border-b">
         <div className="flex justify-end pr-2">
           <LinkButton href="/login" text="Login" />
         </div>
         <h1 className="text-4xl py-2 text-center ">Comunidade de Santa Luzia</h1>
       </main>
+      <CalendarioFixo />
       <Footer />
     </>
   );
