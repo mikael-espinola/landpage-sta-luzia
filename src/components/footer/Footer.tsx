0bcc4222786d344a3ccb1417ddeffb9a04c9c1b3
Mikael Espínola <mikaelespinolaa@gmail.com>
2025-04-10 14:51:57 -0300

    :sparkles: feat: footer inicial incrementado

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='m-0.5 border-t py-2 ' >
+            <p className='text-xs'>Endereço: <span>Avenida das Oliveiras, 3939, Conj. Panorama, Natal - RN</span></p>
+            <div>
+                <p className='text-center'>...maps...</p>
+            </div>
+        </div>
+
+    )
+}
+
+export default Footer
